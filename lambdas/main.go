f48225db856239d95955e23d0786fbbeaad79193
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-10 21:54:53 +0530

    imported strings error removed

--- lambdas/main.go
+++ lambdas/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,48 +12,45 @@ type App struct {
 	id string
 }
 
-
-func newApp (id string ) *App {
+func newApp(id string) *App {
 	return &App{
-		id:id,
+		id: id,
 	}
 }
 
-func (app *App) Handler (request events.APIGatewayProxyRequest)(events.APIGatewayProxyResponse,error)  {
-	responseBody := map[string]string {
-		"message":"Hi you have hit this route",
+func (app *App) Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	responseBody := map[string]string{
+		"message": "Hi you have hit this route",
 	}
 
-		responseJSON, err := json.Marshal(responseBody)
-		if err != nil{
-			return events.APIGatewayProxyResponse{
-				StatusCode: http.StatusInternalServerError,
-				Headers: map[string]string{"Content-Type":"application/json"
-				Body:`{"error":"internal server error "}`,
-			}
-			},nil
+	responseJSON, err := json.Marshal(responseBody)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			Body:       `{"error":"internal server error"}`,
+		}, nil
 	}
+
 	response := events.APIGatewayProxyResponse{
-		Body:strings(responseJSON),
+		Body:       string(responseJSON),
 		StatusCode: http.StatusOK,
 		Headers: map[string]string{
-			"Content-Type":     "text/plain",
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Headers": "Content-Type",
-			"Access-Control-Allow-Methods": "OPTIONS, POST, GET"
+			"Content-Type":                     "text/plain",
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "Content-Type",
+			"Access-Control-Allow-Methods":     "OPTIONS, POST, GET",
 			"Access-Control-Allow-Credentials": "true",
 		},
 		// these are needed for cors
 
-
 	}
-	return response,nil
+	return response, nil
 }
 
-
-func main(){
+func main() {
 	id := "some random string"
-	app := newApp(id) 
+	app := newApp(id)
 	// pass in the handlers
 	lambda.Start(app.Handler)
 }
