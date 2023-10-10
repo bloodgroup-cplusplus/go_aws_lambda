f0378b1c057ed522ddb03b77289dde6deb2f9cd6
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-10 19:24:15 +0530

    creating request and response for servers

--- lambdas/main.go
+++ lambdas/main.go
@@ -1,1 +1,49 @@
+package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type App struct {
+	id string
+}
+
+
+func newApp (id string ) *App {
+	return &App{
+		id:id,
+	}
+}
+
+func (app *App) Handler (request events.APIGatewayProxyRequest)(events.APIGatewayProxyResponse,error)  {
+	responseBody := map[string]string {
+		"message":"Hi you have hit this route",
+	}
+
+		responseJSON, err := json.Marshal(responseBody)
+		if err != nil{
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+				Headers: map[string]string{"Content-Type":"application/json"
+				Body:`{"error":"internal server error "}`,
+			}
+			},nil
+	}
+	response := events.APIGatewayProxyResponse{
+		Body:strings(responseJSON)
+	}
+	return nil
+}
+
+
+func main(){
+	id := "some random string"
+	app := newApp(id) 
+	// pass in the handlers
+	lambda.Start(app.Handler)
+}
